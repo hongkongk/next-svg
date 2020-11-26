df4d9047259f3f3eaa20e62c97cbb5ccbf3fbe4b
hongkongk <hongcongc@gmail.com>
2020-11-26 13:30:44 +0700

    update

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import { HomepageCounter } from '@containers/counter/HomepageCounter'
+import Link from 'next/link'
+import Image from 'next/image'
+
+/**
+ * Homepage
+ */
+const HomePage = () => {
+  return (
+    <main>
+      <h1>Hello, world!</h1>
+      <Link href={'/reset'}>
+        <a>reset page</a>
+      </Link>
+      <Image
+        src={require('@common/img/Biden_2.jpeg')}
+        alt="Picture"
+        width={500}
+        height={500}
+      />
+      <HomepageCounter />
+    </main>
+  )
+}
+
+export default HomePage
